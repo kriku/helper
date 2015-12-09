68be56a23bf5a7cde7379324402412ccadd2b51a
Gosha Krikun <gosha@krikun.org>
2015-12-09 06:26:40 +0300

    remove toggle, remove everything else code

--- data/helper.js
+++ data/helper.js
@@ -1,57 +1,7 @@
 // ---------------------------------------------------------------------
-//	local storage
-// ---------------------------------------------------------------------
-/*
-var field = 'text';
-var url = 'https://kriku.github.io/helper/text.txt';
-
-var initLocalStorage = function(field, url) {
-	if (window.localStorage.getItem(field) == null) {
-		var r = new XMLHttpRequest();
-		r.open('GET', url, true);
-		r.onreadystatechange = function () {
-			if (r.readyState != 4 || r.status != 200) return;
-			console.log('Success: ' + r.responseText);
-			window.localStorage.setItem(field, r.responseText);
-		};
-		r.send();
-	}
-}
-
-initLocalStorage(field, url);
-*/
-
-// ---------------------------------------------------------------------
-//	text analysis
-// ---------------------------------------------------------------------
-
-//TODO add textAnalysis
-/*
-var textAnalysis = function (word, text) {
-	var textL = text.length;
-	var wordL = word.length;
-
-	var offsets = [];
-
-	for (var i=0; i < textL; i++) {
-		var right = 0;
-		for (var j=0; j < wordL; j++) {
-			if (text[i] == word[j]) right++;
-			if (right/wordL > 0.7) break;
-			if ((right-j)/wordL > 0.1) break;
-		}
-		if (right/wordL >= 0.7) offsets.push(i);
-	}
-
-	return offsets;
-}
-*/
-
-// ---------------------------------------------------------------------
 //	event listener on keypress
 // ---------------------------------------------------------------------
 var variants = {};
-var toggle = false;
 var url = "http://q.test/";
 
 var textNodesUnder = function (el) {
@@ -84,48 +34,41 @@ var POST = function (text) {
                 }
             }
         }
-			  //window.localStorage.setItem(field, r.responseText);
 		};
 		r.send(text);
 };
 
 
 document.addEventListener('keypress', function (event) {
-	if (!toggle) {
+    if (event.shiftKey && event.charCode == 65) {
+        var selObj = window.getSelection();
+        var text = selObj.toString();
 
-		if (event.shiftKey && event.charCode == 65) {
-		    var selObj = window.getSelection();
-		    var text = selObj.toString();
-
-		    var selParent = selObj.anchorNode.parentNode;
+        var selParent = selObj.anchorNode.parentNode;
 
         variants = {};
-		    variants.length = 0;
+        variants.length = 0;
         var prev;
-		    while ((variants.length <= 0) && (selParent != window)) {
+        while ((variants.length <= 0) && (selParent != window)) {
             prev = selParent;
-		        selParent = selParent.parentNode;
-		        variants = selParent.getElementsByTagName('input');
-		    }
+            selParent = selParent.parentNode;
+            variants = selParent.getElementsByTagName('input');
+        }
 
         var variantsText = [];
-		    for (var i in variants) {
+        for (var i in variants) {
             if ((variants[i].type == 'radio') || (variants[i].type == 'checkbox')) {
                 if (variants[i].parentNode) {
                     var variantText = textUnder(variants[i].parentNode);
                     variantsText.push(variantText.trim());
                 }
             }
-		    }
+        }
 
         POST('data=' + JSON.stringify({
             "question": textUnder(prev),
             "answers": variantsText}));
-		}
-		toggle = true;
-	} else {
-		toggle = false;
-	}
+    }
 });
 
 
